16557b790cdf0228548582fa02617af00690758b
Ben Walley <benwalleyorigami@gmail.com>
2016-12-05 12:41:32 -0600

    working on home page

---
+++ website/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Yo-Bro Studios</title>
+
+	<!-- fonts  -->
+	<link href="https://fonts.googleapis.com/css?family=Andada|Aref+Ruqaa|Fenix" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Droid+Sans|Roboto|Source+Sans+Pro" rel="stylesheet">
+
+<!-- jQuery -->
+	<script   src="https://code.jquery.com/jquery-3.1.0.min.js"   integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>
+	<!-- css stylesheets -->
+	<link rel="stylesheet" type="text/css" href="css/home.css">
+	
+
+	<?php include("includes/header.html"); ?>
+
+	<!-- Normal content -->
+
+	<div id = "jokeContainer">
+		<div id = "backCover">
+			<p id = "jokeTitle">Joke of the Day!</p>
+			<div id = "jokeText"></div>
+			<a href="lester" id = "moreJokes">Wanna see more awesome jokes <i class="fa fa-angle-double-right" aria-hidden="false"></i></a>
+		</div>
+	</div>
+
+
+
+
+
+<script type="text/javascript" src = "js/homePage.js"></script>
+</body>
+</html>
